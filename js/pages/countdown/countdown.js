94efb19422cfc6c582e443ce64c32a1f8bff0b24
ucyuunaka <cuiylin@mail2.sysu.edu.cn>
2025-04-23 13:01:10 +0800

    更新倒计时页面脚本路径

---
+++ js/pages/countdown/countdown.js
@@ -0,0 +1,7 @@
+// 考试倒计时页面入口
+import { initCountdown } from '/js/pages/countdown/countdown_controller.js';
+
+document.addEventListener("DOMContentLoaded", function() {
+  // 初始化考试倒计时模块
+  initCountdown();
+});
